1b441c9645e3e08d7d942d6c026fe04fdc30592f
whittkilburn <whitt.kilburn@gmail.com>
2025-10-14 17:01:58 -0400

    initial data commit

---
+++ R/ces24.R
@@ -0,0 +1,12 @@
+#' Cooperative Election Study (CES) 2024 Dataset
+#'
+#' This dataset contains responses from the 2024 CES survey,
+#' with 60000 unweighted respondents and 694 variables.
+#'
+#' @format A data frame with 6000 rows and 694 variables:
+#' \describe{
+#'   \item{case_id}{Unique respondent ID}
+#'   ...
+#' }
+#' @source Cooperative Election Study, 2024
+"ces24"
